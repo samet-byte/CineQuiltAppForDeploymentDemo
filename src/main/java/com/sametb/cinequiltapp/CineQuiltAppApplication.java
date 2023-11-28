94523ebfc49cf259c19de2bfd3c7c7efdf9dbe46
samet-byte <sametbayat@hacettepe.edu.tr>
2023-11-28 03:28:31 +0300

    Initial commit

---
+++ src/main/java/com/sametb/cinequiltapp/CineQuiltAppApplication.java
@@ -0,0 +1,13 @@
+package com.sametb.cinequiltapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineQuiltAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CineQuiltAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sametb/cinequiltapp/CineQuiltAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sametb.cinequiltapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineQuiltAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
